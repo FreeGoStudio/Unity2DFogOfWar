5154efa7a6c14294dd00c4d8803fc7788d0147e5
Jacky Jiang <765643729@163.com>
2025-03-09 05:16:47 +0800

    修改设置大小的算法

--- Assets/FogOfWar/Scenes/Light2D&Fov&Fow/FoWManger.cs
+++ Assets/FogOfWar/Scenes/Light2D&Fov&Fow/FoWManger.cs
@@ -3,17 +3,17 @@ using UnityEngine.UI;
 
 public class FOWManger : MonoBehaviour
 {
+    [SerializeField] private int m_PixelsPerWorldUnit = 16;
+
     //FOV
     [SerializeField] private GameObject m_FOVGameObject;
     [SerializeField] private Camera m_FOVCamera;
-    [SerializeField] private float m_FOVCameraSize = 8f;
-    [SerializeField] private int m_FOVCameraRenderTextureSize = 256;
+    [SerializeField] private int m_FOVCameraSize = 8;
     private RenderTexture m_FOVCameraRenderTexture;
 
     //FOW
     [SerializeField] private RawImage m_FOWMask;
-    [SerializeField] private float m_FOWMaskSize = 128;
-    [SerializeField] private int m_FOWMaskRenderTextureSize = 2048;
+    [SerializeField] private int m_FOWMaskSize = 128;
     private RenderTexture m_FOWMaskRenderTexture;
 
     [SerializeField] private ComputeShader m_AddFOVToFOWComputeShader;
@@ -22,7 +22,8 @@ public class FOWManger : MonoBehaviour
 
     private void Awake()
     {
-        m_FOVCameraRenderTexture = new RenderTexture(m_FOVCameraRenderTextureSize, m_FOVCameraRenderTextureSize, 0);
+        int FOVCameraRenderTextureSize = m_PixelsPerWorldUnit * m_FOVCameraSize * 2;
+        m_FOVCameraRenderTexture = new RenderTexture(FOVCameraRenderTextureSize, FOVCameraRenderTextureSize, 0);
         m_FOVCameraRenderTexture.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8_SNorm;
         m_FOVCameraRenderTexture.depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.D16_UNorm;
         m_FOVCameraRenderTexture.filterMode = FilterMode.Point;
@@ -30,6 +31,7 @@ public class FOWManger : MonoBehaviour
         m_FOVCameraRenderTexture.enableRandomWrite = true;
         m_FOVCameraRenderTexture.Create();
 
+        int m_FOWMaskRenderTextureSize = m_FOWMaskSize * m_PixelsPerWorldUnit;
         m_FOWMaskRenderTexture = new RenderTexture(m_FOWMaskRenderTextureSize, m_FOWMaskRenderTextureSize, 0);
         m_FOWMaskRenderTexture.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8_SNorm;
         m_FOWMaskRenderTexture.depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.None;
@@ -42,6 +44,7 @@ public class FOWManger : MonoBehaviour
 
     void Start()
     {
+        Debug.Log(m_FOVCameraSize);
         m_FOVCamera.aspect = 1.0f;
         m_FOVCamera.orthographic = true;
         m_FOVCamera.orthographicSize = m_FOVCameraSize;
@@ -61,16 +64,16 @@ public class FOWManger : MonoBehaviour
         // 获取角色的世界坐标
         var worldPosition = m_FOVGameObject.transform.position;
 
-        var FOVPixelsPerWorldUnit = m_FOVCameraRenderTextureSize / (m_FOVCameraSize * 2f);
+        //var FOVPixelsPerWorldUnit = m_FOVCameraRenderTextureSize / (m_FOVCameraSize * 2f);
 
         // 世界坐标转换成RenderTexture坐标, 1世界坐标=16像素
         Vector2Int renderTexturePos = new Vector2Int(
-            (int)(worldPosition.x * FOVPixelsPerWorldUnit),
-            (int)(worldPosition.y * FOVPixelsPerWorldUnit)
+            (int)(worldPosition.x * m_PixelsPerWorldUnit),
+            (int)(worldPosition.y * m_PixelsPerWorldUnit)
         );
 
         //坐标对齐到遮罩的中心
-        int maskOffset = (int)((m_FOWMaskSize / 2 - m_FOVCameraSize) * m_FOWMaskRenderTextureSize / m_FOWMaskSize);
+        int maskOffset = (int)((m_FOWMaskSize / 2 - m_FOVCameraSize) * m_PixelsPerWorldUnit);
 
         // 将纹理坐标传递给计算着色器
         m_AddFOVToFOWComputeShader.SetInts("Offset", renderTexturePos.x + maskOffset, renderTexturePos.y + maskOffset);
